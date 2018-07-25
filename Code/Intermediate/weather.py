edf32db89a8180c2fca173d5376db5892e53035c
mikeqfu <qian.fu@outlook.com>
2018-07-26 00:06:33 +0100

    Added functions for reading radiation data

--- Code/Intermediate/weather.py
+++ Code/Intermediate/weather.py
@@ -50,8 +50,18 @@ def find_square_corners(centre_point, side_length=5000, rotation=None):
     return lower_left, upper_left, upper_right, lower_right
 
 
+# ====================================================================================================================
+""" UKCP gridded data: temperature and rainfall """
+
+
 # Read gridded weather observations from the raw zipped file
-def read_daily_gridded_weather_obs(filename, col_name='variable_name', start_date='2006-01-01'):
+def read_daily_gridded_weather_obs(filename, col_name, start_date):
+    """
+    :param filename:
+    :param col_name: [str] Variable name, e.g. 'Maximum_Temperature', 'Minimum_Temperature', 'Rainfall'
+    :param start_date: [str] The start date from which the observation data was collected, formatted as 'yyyy-mm-dd'
+    :return:
+    """
     # Centres
     cartesian_centres_temp = pd.read_csv(filename, header=None, index_col=0, nrows=2)
     cartesian_centres = [tuple(x) for x in cartesian_centres_temp.T.values]
@@ -59,7 +69,7 @@ def read_daily_gridded_weather_obs(filename, col_name='variable_name', start_dat
     # Temperature observations
     timeseries_data = pd.read_csv(filename, header=None, skiprows=[0, 1], parse_dates=[0], dayfirst=True)
     timeseries_data[0] = timeseries_data[0].map(lambda x: x.date())
-    if start_date is not None:
+    if start_date is not None and isinstance(pd.to_datetime(start_date), pd.Timestamp):
         mask = (timeseries_data[0] >= pd.to_datetime(start_date).date())
         timeseries_data = timeseries_data.loc[mask]
     timeseries_data.set_index(0, inplace=True)
@@ -84,37 +94,41 @@ def read_daily_gridded_weather_obs(filename, col_name='variable_name', start_dat
 
 
 # Get gridded weather observations
-def get_daily_gridded_weather_obs(filename, col_name='variable_name', start_date='2006-01-01', update=False):
+def get_daily_gridded_weather_obs(filename, col_name, start_date, update=False):
     """
 
     :param filename:
-    :param col_name:
-    :param start_date:
+    :param col_name: [str] Variable name, e.g. 'Maximum_Temperature', 'Minimum_Temperature', 'Rainfall'
+    :param start_date: start_date: [str] The start date from which the observation data was collected; 'yyyy-mm-dd'
     :param update:
     :return:
     """
-    path_to_file = cdd_weather("UKCP gridded data", filename + "_{}".format(start_date.replace("-", "")) + ".pickle")
+    filename_suffix = "" if start_date is None else "_{}".format(start_date.replace("-", ""))
+    pickle_filename = filename + filename_suffix + ".pickle"
+    path_to_pickle = cdd_weather("UKCP gridded obs", pickle_filename)
 
-    if os.path.isfile(path_to_file) and not update:
-        gridded_obs = load_pickle(path_to_file)
+    if os.path.isfile(path_to_pickle) and not update:
+        gridded_obs = load_pickle(path_to_pickle)
     else:
-        path_to_zip_file = cdd_weather("UKCP gridded data", filename.replace('_', ' ').capitalize() + ".zip")
+        path_to_zip = cdd_weather("UKCP gridded obs", filename.replace('_', ' ').capitalize() + ".zip")
 
-        with zipfile.ZipFile(path_to_zip_file, 'r') as zf:
+        with zipfile.ZipFile(path_to_zip, 'r') as zf:
             filename_list = natsort.natsorted(zf.namelist())
             temp_dat = [read_daily_gridded_weather_obs(zf.open(f), col_name, start_date) for f in filename_list]
-
+        zf.close()
         gridded_obs = pd.concat(temp_dat, axis=0, sort=False)
 
-        save_pickle(gridded_obs, path_to_file)
+        save_pickle(gridded_obs, path_to_pickle)
 
     return gridded_obs
 
 
 # Combine weather observations of different variables
-def integrate_daily_gridded_weather_obs(update=False, start_date='2006-01-01'):
-    filename = "daily_gridded_weather_obs_{}".format(start_date.replace('-', ''))
-    path_to_file = cdd_weather("UKCP gridded data", filename + ".pickle")
+def integrate_daily_gridded_weather_obs(start_date='2006-01-01', update=False):
+    assert isinstance(pd.to_datetime(start_date), pd.Timestamp) or start_date is None
+    filename_suffix = "" if start_date is None else "_{}".format(start_date.replace("-", ""))
+    pickle_filename = "daily_gridded_weather_obs{}.pickle".format(filename_suffix)
+    path_to_file = cdd_weather("UKCP gridded obs", pickle_filename)
     if os.path.isfile(path_to_file) and not update:
         daily_gridded_weather_obs = load_pickle(path_to_file)
     else:
@@ -137,19 +151,96 @@ def integrate_daily_gridded_weather_obs(update=False, start_date='2006-01-01'):
     return daily_gridded_weather_obs
 
 
+# ====================================================================================================================
+""" Met Office RADTOB (Radiation values currently being reported) """
+
+
 # Met station locations
-def get_met_station_locations(update=False):
-    spreadsheet_filename = "Met station locations.xlsx"
-    path_to_spreadsheet = cdd_weather(spreadsheet_filename)
-    path_to_pickle = path_to_spreadsheet.replace(".xlsx", ".pickle")
+def get_meteorological_stations(update=False):
+    pickle_filename = "Meteorological stations.pickle"
+    path_to_pickle = cdd_weather(pickle_filename)
     if os.path.isfile(path_to_pickle) and not update:
-        met_stn_loc = load_pickle(path_to_pickle)
+        met_stations = load_pickle(path_to_pickle)
     else:
         try:
-            met_stn_loc = pd.read_excel(path_to_spreadsheet)
-            met_stn_loc.Name = met_stn_loc.Name.str.strip()
-            save_pickle(met_stn_loc, path_to_pickle)
+            met_stations = pd.read_excel(path_to_pickle.replace(".pickle", ".xlsx"), parse_dates=['Station start date'])
+            met_stations.columns = [x.replace(' ', '_').upper() for x in met_stations.columns]
+            met_stations = met_stations.applymap(lambda x: x.strip() if isinstance(x, str) else x)
+            save_pickle(met_stations, path_to_pickle)
         except Exception as e:
-            print("Failed to get {} due to {}".format(spreadsheet_filename, e))
-            met_stn_loc = None
-    return met_stn_loc
+            print("Failed to get {} due to {}".format(pickle_filename, e))
+            met_stations = pd.DataFrame()
+    return met_stations
+
+
+# Read each txt file of MIDAS RADTOB
+def read_radiation_data(filename, headers, full_data=False):
+    """
+    :param filename:
+    :param headers:
+    :param full_data:
+    :return:
+
+    SRC_ID:         Unique source identifier or station site number
+    OB_END_TIME:    Date and time at end of observation
+    OB_HOUR_COUNT:  Observation hour count
+    VERSION_NUM:    Observation version number - Use the row with '1', which has been quality checked by the Met Office
+    GLBL_IRAD_AMT:  Global solar irradiation amount Kjoules/sq metre over the observation period
+
+    """
+    raw_txt = pd.read_csv(filename, header=None, names=headers, parse_dates=[2, 12], dtype={8: str, 10: str, 13: str})
+    if full_data:
+        ro_data = raw_txt
+    else:
+        use_dat = raw_txt[raw_txt.OB_HOUR_COUNT == 24]
+        use_dat.index = range(len(use_dat))
+        ro_data = use_dat[['SRC_ID', 'OB_END_TIME', 'OB_HOUR_COUNT', 'VERSION_NUM', 'GLBL_IRAD_AMT']]
+
+        checked = ro_data.groupby(['SRC_ID', 'OB_END_TIME']).agg('count')
+        checked_idx = checked[checked.VERSION_NUM == 2]
+
+        idx = [ro_data[(ro_data.SRC_ID == i) & (ro_data.OB_END_TIME == d)].sort_values('VERSION_NUM').index[0]
+               for i, d in checked_idx.index]
+        ro_data.drop(idx, axis='index', inplace=True)
+        ro_data.index = range(len(ro_data))
+
+    return ro_data
+
+
+# Headers of the midas_radtob data set
+def get_ro_headers():
+    headers_raw = pd.read_excel(cdd_weather("Radiation obs", "RO_Column_Headers.xlsx"), header=None)
+    headers = [x.strip() for x in headers_raw.iloc[0, :].values]
+    return headers
+
+
+# MIDAS RADTOB
+def get_midas_radtob(full_data=False, update=False):
+    """
+    :param full_data:
+    :param update:
+    :return:
+
+    MIDAS   -   Met Office Integrated Data Archive System
+    RADTOB 	-   RADT-OB table. Radiation values currently being reported
+
+    """
+    pickle_filename = "midas_radtob_20060101_20141231{}.pickle".format("_full" if full_data else "")
+    path_to_pickle = cdd_weather("Radiation obs", pickle_filename)
+    if os.path.isfile(path_to_pickle) and not update:
+        radiation_data = load_pickle(path_to_pickle)
+    else:
+        headers = get_ro_headers()
+        try:
+            path_to_zip = path_to_pickle.replace("_full.pickle" if full_data else ".pickle", ".zip")
+            with zipfile.ZipFile(path_to_zip, 'r') as zf:
+                filename_list = natsort.natsorted(zf.namelist())
+                temp_dat = [read_radiation_data(zf.open(f), headers, full_data) for f in filename_list]
+            zf.close()
+            radiation_data = pd.concat(temp_dat, axis=0, sort=False, ignore_index=True)
+            save_pickle(radiation_data, path_to_pickle)
+        except Exception as e:
+            print("Failed to get \"Radiation obs\".")
+            print(e)
+            radiation_data = pd.DataFrame(columns=headers)
+    return radiation_data
