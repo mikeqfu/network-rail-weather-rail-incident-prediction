ddb39714a1a3facc1be2da6a7cc09b3e42c9ac3c
mikeqfu <qian.fu@outlook.com>
2018-08-12 03:06:46 +0100

    Fixed bugs

--- Code/railwaycodes_utils.py
+++ Code/railwaycodes_utils.py
@@ -199,7 +199,9 @@ def parse_loc_note(x):
     elif ' [unknown feature, labelled "do not use"]' in x:
         dat = re.search('\w+(?= \[unknown feature, )', x).group()
     else:
-        m_pattern = re.compile('[Oo]riginally |[Ff]ormerly |[Ll]ater |[Pp]resumed | \(was | \(in | \(at |\?|\"|\n')
+        m_pattern = re.compile(
+            '[Oo]riginally |[Ff]ormerly |[Ll]ater |[Pp]resumed | \(was | \(in | \(at | \(also known as |'
+            ' \(second code set |\?|\"|\n')
         # dat = re.search('["\w ,]+(?= [[(?\'])|["\w ,]+', x).group(0) if re.search(m_pattern, x) else x
         dat = ' '.join(x.replace(x[x.find('('):x.find(')') + 1], '').split()) if re.search(m_pattern, x) else x
     # Note
