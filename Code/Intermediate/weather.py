16697c79f97a911403c56ac2f45ee7498c399f25
mikeqfu <qian.fu@outlook.com>
2018-08-20 19:37:34 +0100

    Created new functions and updated functions for reading gridded obs data

--- Code/Intermediate/weather.py
+++ Code/Intermediate/weather.py
@@ -1,6 +1,5 @@
 """ Gridded weather observations """
 
-import itertools
 import os
 import zipfile
 
@@ -21,7 +20,7 @@ def cdd_weather(*directories):
 
 
 # Find coordinates for each corner of the weather observation grid
-def find_square_corners(centre_point, side_length=5000, rotation=None):
+def create_grid(centre_point, side_length=5000, rotation=None):
     """
 
     :param centre_point: (easting, northing)
@@ -55,11 +54,59 @@ def find_square_corners(centre_point, side_length=5000, rotation=None):
 
 
 # ====================================================================================================================
-""" UKCP gridded data: temperature and rainfall """
+""" UKCP gridded data: maximum/minimum temperatures and rainfall """
+
+
+# Read observation grids
+def read_observation_grids(obs_filename):
+    cartesian_centres_temp = pd.read_csv(obs_filename, header=None, index_col=0, nrows=2)
+    cartesian_centres = [tuple(x) for x in cartesian_centres_temp.T.values]
+
+    grid = [create_grid(centre, 5000, rotation=None) for centre in cartesian_centres]
+
+    long_lat = [osgb36_to_wgs84(x[0], x[1]) for x in cartesian_centres]
+
+    obs_grids = pd.DataFrame({'Centroid': cartesian_centres,
+                              'Centroid_XY': [shapely.geometry.Point(x) for x in cartesian_centres],
+                              'Centroid_LongLat': [shapely.geometry.Point(x) for x in long_lat],
+                              'Grid': [shapely.geometry.Polygon(x) for x in grid]})
+
+    return obs_grids
+
+
+# Get all observation grids
+def get_observation_grids(obs_zip_filename="daily-rainfall.zip", update=False):
+    pickle_filename = "observation-grids.pickle"
+    path_to_pickle = cdd_weather("UKCP gridded obs", pickle_filename)
+
+    if os.path.isfile(path_to_pickle) and not update:
+        observation_grids = load_pickle(path_to_pickle)
+    else:
+        try:
+            path_to_zip = cdd_weather("UKCP gridded obs", obs_zip_filename)
+
+            with zipfile.ZipFile(path_to_zip, 'r') as zf:
+                filename_list = natsort.natsorted(zf.namelist())
+                obs_grids = [read_observation_grids(zf.open(f)) for f in filename_list]
+            zf.close()
+
+            observation_grids = pd.concat(obs_grids, ignore_index=True)
+
+            # Add a pseudo id for each observation grid
+            observation_grids = observation_grids.sort_values('Centroid').set_index('Centroid')
+            observation_grids['Pseudo_Grid_ID'] = range(len(observation_grids))
+
+            save_pickle(observation_grids, path_to_pickle)
+
+        except Exception as e:
+            print("Failed to get \"Observation Grids\". {}".format(e))
+            observation_grids = pd.DataFrame()
+
+    return observation_grids
 
 
 # Read gridded weather observations from the raw zipped file
-def read_daily_gridded_weather_obs(filename, col_name, start_date):
+def read_daily_gridded_weather_obs(filename, col_name, start_date='2006-01-01'):
     """
     :param filename:
     :param col_name: [str] Variable name, e.g. 'Maximum_Temperature', 'Minimum_Temperature', 'Rainfall'
@@ -79,34 +126,34 @@ def read_daily_gridded_weather_obs(filename, col_name, start_date):
     timeseries_data.set_index(0, inplace=True)
 
     # Reshape the dataframe
-    idx = pd.MultiIndex.from_product([cartesian_centres, timeseries_data.index.tolist()], names=['Centroid_XY', 'Date'])
+    idx = pd.MultiIndex.from_product([cartesian_centres, timeseries_data.index.tolist()], names=['Centroid', 'Date'])
     data = pd.DataFrame(timeseries_data.T.values.flatten(), index=idx, columns=[col_name])
-    data.reset_index(inplace=True)
+    # data.reset_index(inplace=True)
 
     # data.Centre = data.Centre.map(shapely.geometry.asPoint)
 
-    # Add levels of Grid corners (and centres' LongLat)
-    num = len(timeseries_data)
+    # # Add levels of Grid corners (and centres' LongLat)
+    # num = len(timeseries_data)
 
-    grid = [find_square_corners(centre, 5000, rotation=None) for centre in cartesian_centres]
-    data['Grid_Vertices'] = list(itertools.chain.from_iterable(itertools.repeat(x, num) for x in grid))
+    # import itertools
 
-    long_lat = [osgb36_to_wgs84(x[0], x[1]) for x in cartesian_centres]
-    data['Centroid_LongLat'] = list(itertools.chain.from_iterable(itertools.repeat(x, num) for x in long_lat))
+    # grid = [find_square_corners(centre, 5000, rotation=None) for centre in cartesian_centres]
+    # data['Grid'] = list(itertools.chain.from_iterable(itertools.repeat(x, num) for x in grid))
 
-    data.set_index(['Grid_Vertices', 'Centroid_XY', 'Centroid_LongLat', 'Date'], inplace=True)
+    # long_lat = [osgb36_to_wgs84(x[0], x[1]) for x in cartesian_centres]
+    # data['Centroid_LongLat'] = list(itertools.chain.from_iterable(itertools.repeat(x, num) for x in long_lat))
 
     return data
 
 
 # Get gridded weather observations
-def get_daily_gridded_weather_obs(filename, col_name, start_date, update=True):
+def get_daily_gridded_weather_obs(filename, col_name, start_date='2006-01-01', pseudo_grid_id=False, update=False):
     """
-
     :param filename:
     :param col_name: [str] Variable name, e.g. 'Maximum_Temperature', 'Minimum_Temperature', 'Rainfall'
     :param start_date: start_date: [str] The start date from which the observation data was collected; 'yyyy-mm-dd'
-    :param update:
+    :param pseudo_grid_id: [bool]
+    :param update: [bool]
     :return:
     """
     filename_suffix = "" if start_date is None else "-{}".format(start_date.replace("-", ""))
@@ -116,23 +163,33 @@ def get_daily_gridded_weather_obs(filename, col_name, start_date, update=True):
     if os.path.isfile(path_to_pickle) and not update:
         gridded_obs = load_pickle(path_to_pickle)
     else:
+        try:
+            path_to_zip = cdd_weather("UKCP gridded obs", filename + ".zip")
 
-        path_to_zip = cdd_weather("UKCP gridded obs", filename + ".zip")
+            with zipfile.ZipFile(path_to_zip, 'r') as zf:
+                filename_list = natsort.natsorted(zf.namelist())
+                obs_data = [read_daily_gridded_weather_obs(zf.open(f), col_name, start_date) for f in filename_list]
+            zf.close()
 
-        with zipfile.ZipFile(path_to_zip, 'r') as zf:
-            filename_list = natsort.natsorted(zf.namelist())
-            temp_dat = [read_daily_gridded_weather_obs(zf.open(f), col_name, start_date) for f in filename_list]
-        zf.close()
+            gridded_obs = pd.concat(obs_data, axis=0)
 
-        gridded_obs = pd.concat(temp_dat, axis=0, sort=False)
+            # Add a pseudo id for each observation grid
+            if pseudo_grid_id:
+                observation_grids = get_observation_grids(update=update)
+                gridded_obs = gridded_obs.reset_index(level='Date').join(observation_grids[['Pseudo_Grid_ID']])
+                gridded_obs.set_index(['Pseudo_Grid_ID', 'Date'], inplace=True)
 
-        save_pickle(gridded_obs, path_to_pickle)
+            save_pickle(gridded_obs, path_to_pickle)
+
+        except Exception as e:
+            print("Failed to get \"{}\". {}.".format(filename, e))
+            gridded_obs = pd.DataFrame()
 
     return gridded_obs
 
 
 # Combine weather observations of different variables
-def get_integrated_daily_gridded_weather_obs(start_date='2006-01-01', update=False):
+def get_integrated_daily_gridded_weather_obs(start_date='2006-01-01', pseudo_grid_id=True, update=False):
 
     assert isinstance(pd.to_datetime(start_date), pd.Timestamp) or start_date is None
 
@@ -141,22 +198,30 @@ def get_integrated_daily_gridded_weather_obs(start_date='2006-01-01', update=Fal
     path_to_file = cdd_weather("UKCP gridded obs", pickle_filename)
 
     if os.path.isfile(path_to_file) and not update:
-        daily_gridded_weather_obs = load_pickle(path_to_file)
+        daily_gridded_obs = load_pickle(path_to_file)
     else:
         try:
-            d_max_temp = get_daily_gridded_weather_obs("daily-maximum-temperature", 'Maximum_Temperature', start_date)
-            d_min_temp = get_daily_gridded_weather_obs("daily-minimum-temperature", 'Minimum_Temperature', start_date)
-            d_rainfall = get_daily_gridded_weather_obs("daily-rainfall", 'Rainfall', start_date)
+            d_max_temp = get_daily_gridded_weather_obs(
+                "daily-maximum-temperature", 'Maximum_Temperature', start_date, pseudo_grid_id=False, update=update)
+            d_min_temp = get_daily_gridded_weather_obs(
+                "daily-minimum-temperature", 'Minimum_Temperature', start_date, pseudo_grid_id=False, update=update)
+            d_rainfall = get_daily_gridded_weather_obs(
+                "daily-rainfall", 'Rainfall', start_date, pseudo_grid_id=False, update=update)
 
-            daily_gridded_weather_obs = pd.concat([d_max_temp, d_min_temp, d_rainfall], axis=1)
+            daily_gridded_obs = pd.concat([d_max_temp, d_min_temp, d_rainfall], axis=1)
 
-            save_pickle(daily_gridded_weather_obs, path_to_file)
+            if pseudo_grid_id:
+                obs_grids = get_observation_grids(update=update)
+                daily_gridded_obs = daily_gridded_obs.reset_index(level='Date').join(obs_grids[['Pseudo_Grid_ID']])
+                daily_gridded_obs.set_index(['Pseudo_Grid_ID', 'Date'], inplace=True)
+
+            save_pickle(daily_gridded_obs, path_to_file)
 
         except Exception as e:
             print("Failed to get integrated daily gridded weather observations. {}.".format(e))
-            daily_gridded_weather_obs = pd.DataFrame()
+            daily_gridded_obs = pd.DataFrame()
 
-    return daily_gridded_weather_obs
+    return daily_gridded_obs
 
 
 # ====================================================================================================================
