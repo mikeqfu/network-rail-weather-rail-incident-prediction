12e01859ec8d1c3ab5acbb4eea93d93d8fba2c33
mikeqfu <qian.fu@outlook.com>
2019-08-15 16:31:24 +0100

    Modify midas.py

--- Code/weather/midas.py
+++ Code/weather/midas.py
@@ -5,56 +5,47 @@ import os
 import zipfile
 
 import natsort
+import numpy as np
 import pandas as pd
-import shapely.geometry
-from pyhelpers.settings import pd_preferences
+from pyhelpers.geom import wgs84_to_osgb36
 from pyhelpers.store import load_pickle, save_pickle
 
+import settings
 from weather.tools import cdd_weather
 
-pd_preferences()
+settings.pd_preferences()
 
 
 # Locations of the meteorological stations ---------------------------------------------------------------------------
-def get_meteorological_stations_locations(update=False) -> pd.DataFrame:
-    """
-    :param update: [bool]
-    """
-    filename = "meteorological-stations"
-    path_to_pickle = cdd_weather(filename + ".pickle")
+def get_radiation_stations_information(update=False) -> pd.DataFrame:
+    filename = "radiation-stations-information"
+    path_to_pickle = cdd_weather("MIDAS", filename + ".pickle")
     if os.path.isfile(path_to_pickle) and not update:
         return load_pickle(path_to_pickle)
     else:
         try:
-            path_to_spreadsheet = cdd_weather(filename + ".xlsx")
-            met_stations_info = pd.read_excel(path_to_spreadsheet, parse_dates=['Station start date'])
+            path_to_spreadsheet = cdd_weather("MIDAS", filename + ".xlsx")
+            rad_stations_info = pd.read_excel(path_to_spreadsheet, parse_dates=['Station start date'])
+            rad_stations_info.columns = [x.title().replace(' ', '') if x != 'src_id' else x.upper()
+                                         for x in rad_stations_info.columns]
+            rad_stations_info.StationName = rad_stations_info.StationName.str.replace('\xa0\xa0\xa0\xa0Locate', '')
 
-            met_stations_info.columns = [x.replace(' ', '_').upper() for x in met_stations_info.columns]
-            met_stations_info = met_stations_info.applymap(lambda x: x.strip() if isinstance(x, str) else x)
+            osgb_en = np.array(wgs84_to_osgb36(rad_stations_info.Longitude.values, rad_stations_info.Latitude.values))
+            rad_stations_info[['Easting', 'Northing']] = pd.DataFrame(osgb_en.T)
 
-            # Convert coordinates to shapely.geometry.Point
-            met_stations_info['LONG_LAT'] = met_stations_info.apply(lambda x: (x['LONGITUDE'], x['LATITUDE']), axis=1)
-            met_stations_info['LONG_LAT_GEOM'] = met_stations_info.apply(
-                lambda x: shapely.geometry.Point((x['LONGITUDE'], x['LATITUDE'])), axis=1)
-            met_stations_info['E_N'] = met_stations_info.apply(lambda x: (x['EASTING'], x['NORTHING']), axis=1)
-            met_stations_info['E_N_GEOM'] = met_stations_info.apply(
-                lambda x: shapely.geometry.Point((x['EASTING'], x['NORTHING'])), axis=1)
+            rad_stations_info.sort_values(['SRC_ID'], inplace=True)
+            rad_stations_info.set_index('SRC_ID', inplace=True)
 
-            met_stations_info.rename(columns={'NAME': 'MET_STATION'}, inplace=True)
+            save_pickle(rad_stations_info, path_to_pickle.replace(".xlsx", ".pickle"))
 
-            met_stations_info.sort_values(['SRC_ID', 'MET_STATION'], inplace=True)
-            met_stations_info.set_index('SRC_ID', inplace=True)
-
-            save_pickle(met_stations_info, path_to_pickle.replace(".xlsx", ".pickle"))
-
-            return met_stations_info
+            return rad_stations_info
 
         except Exception as e:
             print("Failed to get the locations of the meteorological stations. {}".format(e))
 
 
 # MIDAS RADTOB (Radiation data) --------------------------------------------------------------------------------------
-def parse_midas_radtob(filename: str, headers: list, daily=False, met_stn=False):
+def parse_midas_radtob(filename: str, headers: list, daily=False, rad_stn=False):
     """
     MIDAS  - Met Office Integrated Data Archive System
     RADTOB - RADT-OB table. Radiation values currently being reported
@@ -62,7 +53,7 @@ def parse_midas_radtob(filename: str, headers: list, daily=False, met_stn=False)
     :param filename: [str] e.g. filename = "midas_radtob_200601-200612.txt"
     :param headers: [list]
     :param daily: [bool] if True, 'OB_HOUR_COUNT' == 24, i.e. aggregate value in one day 24 hours; False (default)
-    :param met_stn: [bool] if True, add the location of meteorological station; False (default)
+    :param rad_stn: [bool] if True, add the location of meteorological station; False (default)
 
     SRC_ID:        Unique source identifier or station site number
     OB_END_TIME:   Date and time at end of observation
@@ -81,103 +72,80 @@ def parse_midas_radtob(filename: str, headers: list, daily=False, met_stn=False)
         ro_data = ro_data[ro_data.OB_HOUR_COUNT == 24]
         ro_data.index = range(len(ro_data))
 
-    # Rename 'OB_END_TIME'
-    ro_data.rename(columns={'OB_END_TIME': 'OB_END_DATE_TIME'}, inplace=True)
-
     # Cleanse the data
-    key_cols = ['SRC_ID', 'OB_END_DATE_TIME', 'OB_HOUR_COUNT']
+    key_cols = ['SRC_ID', 'OB_END_TIME', 'OB_HOUR_COUNT']
 
-    # Remove records where VERSION_NUM == 0 if higher versions (i.e. VERSION_NUM == 1) are available
-    checked_tmp = ro_data.groupby(key_cols).agg({'VERSION_NUM': 'count'})
-    checked_dat = checked_tmp[checked_tmp.VERSION_NUM >= 2]
-    temp = ro_data.join(checked_dat, on=key_cols, rsuffix='_checked')
-    to_drop_tmp = temp[temp.VERSION_NUM_checked.notnull()]
+    checked_tmp = ro_data.groupby(key_cols).agg({'VERSION_NUM': max})
+    checked_tmp.reset_index(inplace=True)
+    key_cols_ = key_cols + ['VERSION_NUM']
+    ro_data_ = checked_tmp.join(ro_data.set_index(key_cols_), on=key_cols_)
 
-    to_drop_idx = to_drop_tmp[to_drop_tmp.VERSION_NUM == 0].index
-    ro_data.drop(to_drop_idx, axis='index', inplace=True)
+    # Note: The following line is questionable
+    ro_data_.loc[(ro_data_.GLBL_IRAD_AMT < 0.0) | ro_data_.GLBL_IRAD_AMT.isna(), 'GLBL_IRAD_AMT'] = 0.0  # or np.nan
 
-    # Remove records with duplicated "VERSION_NUM == 1"
-    checked_tmp = ro_data.groupby(key_cols).agg({'VERSION_NUM': 'count'})
-    checked_dat = checked_tmp[checked_tmp.VERSION_NUM >= 2]
-    temp = ro_data.join(checked_dat, on=key_cols, rsuffix='_checked')
-    to_drop_tmp = temp[temp.VERSION_NUM_checked.notnull()]
+    ro_data_.sort_values(['SRC_ID', 'OB_END_TIME'], inplace=True)  # Sort rows
+    ro_data_.index = range(len(ro_data_))
 
-    to_drop_idx = to_drop_tmp.drop_duplicates(key_cols, keep='first').index
-    ro_data.drop(to_drop_idx, axis='index', inplace=True)
+    # Insert 'OB_END_DATE'
+    ro_data_.insert(ro_data_.columns.get_loc('OB_END_TIME'), column='OB_END_DATE', value=ro_data_.OB_END_TIME.dt.date)
 
-    # Sort rows
-    ro_data.sort_values(key_cols, inplace=True)
-    ro_data.index = range(len(ro_data))
+    ro_data_.rename(columns={'OB_END_TIME': 'OB_END_DATE_TIME'}, inplace=True)  # Rename 'OB_END_TIME'
 
-    # Insert dates of 'OB_END_DATE_TIME'
-    ob_end_date = ro_data.OB_END_DATE_TIME.map(lambda x: x.date())
-    ro_data.insert(ro_data.columns.get_loc('OB_END_DATE_TIME'), column='OB_END_DATE', value=ob_end_date)
+    if rad_stn:
+        rad_stn_info = get_radiation_stations_information()
+        ro_data_ = ro_data_.join(rad_stn_info, on='SRC_ID')
 
-    if met_stn:
-        met_stn = get_meteorological_stations_locations()
-        met_stn.rename(columns={'NAME': 'MET_STATION'}, inplace=True)
-        ro_data = ro_data.join(met_stn, on='SRC_ID')
-
-    return ro_data
+    return ro_data_
 
 
-def make_radtob_pickle_path(data_filename: str, daily: bool, met_stn: bool):
+def make_radtob_pickle_path(data_filename: str, daily: bool, rad_stn: bool):
     """
     :param data_filename: e.g. data_filename="midas-radtob-20060101-20141231"
     :param daily: e.g. daily=False
-    :param met_stn: e.g. met_stn=False
+    :param rad_stn: e.g. met_stn=False
     :return: [str]
     """
-    filename_suffix = "-agg" if daily else "", "-met_stn" if met_stn else ""
+    filename_suffix = "-agg" if daily else "", "-met_stn" if rad_stn else ""
     path_to_radtob_pickle = cdd_weather("MIDAS", "{}{}.pickle".format(data_filename, *filename_suffix))
     return path_to_radtob_pickle
 
 
-def get_radtob(data_filename="midas-radtob-2006-2019", daily=False, met_stn=False, update=False) -> pd.DataFrame:
+def get_radtob(data_filename="midas-radtob-2006-2019", daily=False, update=False) -> pd.DataFrame:
     """
     :param data_filename: [str]
     :param daily: [bool] if True, 'OB_HOUR_COUNT' == 24, i.e. aggregate value in one day 24 hours; False (default)
-    :param met_stn: [bool] if True, add the location of meteorological station; False (default)
-    :param update:
+    :param update: [bool]
 
     Testing parameters:
     e.g.
         data_filename="midas-radtob-2006-2019"
         daily=False
-        met_stn=False
         update=False
     """
-    path_to_pickle = make_radtob_pickle_path(data_filename, daily, met_stn)
+    path_to_pickle = make_radtob_pickle_path(data_filename, daily, rad_stn=False)
     if os.path.isfile(path_to_pickle) and not update:
         return load_pickle(path_to_pickle)
     else:
         try:
             # Headers of the midas_radtob data set
-            headers_raw = pd.read_excel(cdd_weather("MIDAS", "RO-column-headers.xlsx"), header=None)
+            headers_raw = pd.read_excel(cdd_weather("MIDAS", "radiation-observation-data-headers.xlsx"), header=None)
             headers = [x.strip() for x in headers_raw.iloc[0, :].values]
 
             path_to_zip = cdd_weather("MIDAS", data_filename + ".zip")
             with zipfile.ZipFile(path_to_zip, 'r') as zf:
                 filename_list = natsort.natsorted(zf.namelist())
-                temp_dat = [parse_midas_radtob(zf.open(f), headers, daily, met_stn=False) for f in filename_list]
+                temp_dat = [parse_midas_radtob(zf.open(f), headers, daily, rad_stn=False) for f in filename_list]
             zf.close()
 
             radtob = pd.concat(temp_dat, axis=0, ignore_index=True, sort=False)
-
-            # Note: The following line is questionable
-            radtob.loc[(radtob.GLBL_IRAD_AMT < 0) | radtob.GLBL_IRAD_AMT.isna(), 'GLBL_IRAD_AMT'] = 0  # or pd.np.nan
-
-            # Whether to add information about the associated meteorological stations
-            radtob_ = radtob.join(get_meteorological_stations_locations(), on='SRC_ID') if met_stn else radtob
-
-            radtob_.set_index(['SRC_ID', 'OB_END_DATE'], inplace=True)
+            radtob.set_index(['SRC_ID', 'OB_END_DATE_TIME'], inplace=True)
 
             # Save data as a pickle
-            save_pickle(radtob_, path_to_pickle)
+            save_pickle(radtob, path_to_pickle)
 
             gc.collect()
 
-            return radtob_
+            return radtob
 
         except Exception as e:
             print("Failed to get the radiation observations. {}".format(e))
