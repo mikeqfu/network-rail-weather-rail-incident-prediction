d5ceb47ddb9d779e9a47c66917f92f9748a9ec75
mikeqfu <qian.fu@outlook.com>
2020-11-19 23:07:40 +0000

    optimise import statements and modify docstrings for \weather\midas.py

--- src/weather/midas.py
+++ src/weather/midas.py
@@ -1,4 +1,6 @@
-""" Met Office RADTOB (Radiation values currently being reported) """
+"""
+Met Office RADTOB (Radiation values currently being reported).
+"""
 
 import gc
 import glob
@@ -23,9 +25,11 @@ def get_radiation_stations_information(update=False, verbose=False):
     """
     Get locations and relevant information of meteorological stations.
 
-    :param update: whether to check on update and proceed to update the package data, defaults to ``False``
+    :param update: whether to check on update and proceed to update the package data,
+        defaults to ``False``
     :type update: bool
-    :param verbose: whether to print relevant information in console as the function runs, defaults to ``False``
+    :param verbose: whether to print relevant information in console as the function runs,
+        defaults to ``False``
     :type verbose: bool, int
     :return: data of meteorological stations
     :rtype: pandas.DataFrame
@@ -50,15 +54,18 @@ def get_radiation_stations_information(update=False, verbose=False):
     else:
         try:
             path_to_spreadsheet = path_to_pickle.replace(".pickle", ".xlsx")
-            rad_stations_info = pd.read_excel(path_to_spreadsheet, parse_dates=['Station start date'])
+            rad_stations_info = pd.read_excel(path_to_spreadsheet,
+                                              parse_dates=['Station start date'])
             rad_stations_info.columns = [x.title().replace(' ', '') if x != 'src_id' else x.upper()
                                          for x in rad_stations_info.columns]
-            rad_stations_info.StationName = rad_stations_info.StationName.str.replace(r'(\xa0)+Locate', '', regex=True)
+            rad_stations_info.StationName = \
+                rad_stations_info.StationName.str.replace(r'(\xa0)+Locate', '', regex=True)
 
             rad_stations_info['Easting'], rad_stations_info['Northing'] = wgs84_to_osgb36(
                 rad_stations_info.Longitude.values, rad_stations_info.Latitude.values)
             rad_stations_info['EN_GEOM'] = [
-                shapely.geometry.Point(xy) for xy in zip(rad_stations_info.Easting, rad_stations_info.Northing)]
+                shapely.geometry.Point(xy)
+                for xy in zip(rad_stations_info.Easting, rad_stations_info.Northing)]
 
             rad_stations_info.sort_values(['SRC_ID'], inplace=True)
             rad_stations_info.set_index('SRC_ID', inplace=True)
@@ -82,7 +89,8 @@ def parse_midas_radtob(filename, headers, daily=False, rad_stn=False):
     :type filename: str
     :param headers: column names of the data frame
     :type headers: list
-    :param daily: if True, 'OB_HOUR_COUNT' == 24, i.e. aggregate value in one day 24 hours; False (default)
+    :param daily: if True, 'OB_HOUR_COUNT' == 24, i.e. aggregate value in one day 24 hours;
+        False (default)
     :type daily: bool
     :param rad_stn: if True, add the location of meteorological station; False (default)
     :type rad_stn: bool
@@ -97,8 +105,8 @@ def parse_midas_radtob(filename, headers, daily=False, rad_stn=False):
         GLBL_IRAD_AMT: Global solar irradiation amount Kjoules/sq metre over the observation period
     """
 
-    raw_txt = pd.read_csv(filename, header=None, names=headers, parse_dates=[2, 12], infer_datetime_format=True,
-                          skipinitialspace=True)
+    raw_txt = pd.read_csv(filename, header=None, names=headers, parse_dates=[2, 12],
+                          infer_datetime_format=True, skipinitialspace=True)
 
     ro_data = raw_txt[['SRC_ID', 'OB_END_TIME', 'OB_HOUR_COUNT', 'VERSION_NUM', 'GLBL_IRAD_AMT']]
     ro_data.drop_duplicates(inplace=True)
@@ -116,15 +124,19 @@ def parse_midas_radtob(filename, headers, daily=False, rad_stn=False):
     ro_data_ = checked_tmp.join(ro_data.set_index(key_cols_), on=key_cols_)
 
     # Note: The following line is questionable
-    ro_data_.loc[(ro_data_.GLBL_IRAD_AMT < 0.0) | ro_data_.GLBL_IRAD_AMT.isna(), 'GLBL_IRAD_AMT'] = 0.0  # or np.nan
+    ro_data_.loc[
+        (ro_data_.GLBL_IRAD_AMT < 0.0) | ro_data_.GLBL_IRAD_AMT.isna(), 'GLBL_IRAD_AMT'] = 0.0
+    # or np.nan
 
     ro_data_.sort_values(['SRC_ID', 'OB_END_TIME'], inplace=True)  # Sort rows
     ro_data_.index = range(len(ro_data_))
 
     # Insert 'OB_END_DATE'
-    ro_data_.insert(ro_data_.columns.get_loc('OB_END_TIME'), column='OB_END_DATE', value=ro_data_.OB_END_TIME.dt.date)
+    ro_data_.insert(ro_data_.columns.get_loc('OB_END_TIME'), column='OB_END_DATE',
+                    value=ro_data_.OB_END_TIME.dt.date)
 
-    ro_data_.rename(columns={'OB_END_TIME': 'OB_END_DATE_TIME'}, inplace=True)  # Rename 'OB_END_TIME'
+    # Rename 'OB_END_TIME'
+    ro_data_.rename(columns={'OB_END_TIME': 'OB_END_DATE_TIME'}, inplace=True)
 
     if rad_stn:
         rad_stn_info = get_radiation_stations_information()
@@ -148,21 +160,27 @@ def make_midas_radtob_pickle_path(data_filename, daily, rad_stn):
     """
 
     filename_suffix = "-agg" if daily else "", "-met_stn" if rad_stn else ""
-    path_to_radtob_pickle = cdd_weather("MIDAS", "{}{}.pickle".format(data_filename, *filename_suffix))
+    path_to_radtob_pickle = cdd_weather(
+        "MIDAS", "{}{}.pickle".format(data_filename, *filename_suffix))
+
     return path_to_radtob_pickle
 
 
-def get_midas_radtob(data_filename="midas-radtob-2006-2019", daily=False, update=False, verbose=False):
+def get_midas_radtob(data_filename="midas-radtob-2006-2019", daily=False, update=False,
+                     verbose=False):
     """
     Get MIDAS RADTOB (Radiation data).
 
     :param data_filename: defaults to ``"midas-radtob-2006-2019"``
     :type data_filename: str
-    :param daily: if True, 'OB_HOUR_COUNT' == 24, i.e. aggregate value in one day 24 hours; False (default)
+    :param daily: if True, 'OB_HOUR_COUNT' == 24, i.e. aggregate value in one day 24 hours;
+        defaults to ``False``
     :type daily: bool
-    :param update: whether to check on update and proceed to update the package data, defaults to ``False``
+    :param update: whether to check on update and proceed to update the package data,
+        defaults to ``False``
     :type update: bool
-    :param verbose: whether to print relevant information in console as the function runs, defaults to ``False``
+    :param verbose: whether to print relevant information in console as the function runs,
+        defaults to ``False``
     :type verbose: bool, int
     :return: MIDAS RADTOB (Radiation data)
     :rtype: pandas.DataFrame
@@ -187,13 +205,15 @@ def get_midas_radtob(data_filename="midas-radtob-2006-2019", daily=False, update
     else:
         try:
             # Headers of the midas_radtob data set
-            headers_raw = pd.read_excel(cdd_weather("midas", "radiation-observation-data-headers.xlsx"), header=None)
+            headers_raw = pd.read_excel(
+                cdd_weather("midas", "radiation-observation-data-headers.xlsx"), header=None)
             headers = [x.strip() for x in headers_raw.iloc[0, :].values]
 
             path_to_zip = cdd_weather("midas", data_filename + ".zip")
             with zipfile.ZipFile(path_to_zip, 'r') as zf:
                 filename_list = natsort.natsorted(zf.namelist())
-                temp_dat = [parse_midas_radtob(zf.open(f), headers, daily, rad_stn=False) for f in filename_list]
+                temp_dat = [parse_midas_radtob(zf.open(f), headers, daily, rad_stn=False)
+                            for f in filename_list]
             zf.close()
 
             radtob = pd.concat(temp_dat, axis=0, ignore_index=True, sort=False)
@@ -208,8 +228,8 @@ def get_midas_radtob(data_filename="midas-radtob-2006-2019", daily=False, update
             print("Failed to get the radiation observations. {}".format(e))
 
 
-def dump_midas_radtob_to_mssql(table_name='MIDAS_RADTOB', if_exists='append', chunk_size=100000, update=False,
-                               verbose=False):
+def dump_midas_radtob_to_mssql(table_name='MIDAS_RADTOB', if_exists='append', chunk_size=100000,
+                               update=False, verbose=False):
     """
     See also [`DUDTM <https://stackoverflow.com/questions/50689082>`_].
 
@@ -244,9 +264,10 @@ def dump_midas_radtob_to_mssql(table_name='MIDAS_RADTOB', if_exists='append', ch
     temp_file_ = pd.read_csv(csv_filename, chunksize=tsql_chunksize)
     for chunk in temp_file_:
         # e.g. chunk = temp_file_.get_chunk(tsql_chunksize)
-        chunk.to_sql(table_name, midas_radtob_engine, schema='dbo', if_exists=if_exists, index=False,
-                     dtype={'OB_END_DATE_TIME': sqlalchemy.types.DATETIME, 'OB_END_DATE': sqlalchemy.types.DATE},
-                     method='multi')
+        dtype_ = {'OB_END_DATE_TIME': sqlalchemy.types.DATETIME,
+                  'OB_END_DATE': sqlalchemy.types.DATE}
+        chunk.to_sql(table_name, midas_radtob_engine, schema='dbo', if_exists=if_exists,
+                     index=False, dtype=dtype_, method='multi')
         gc.collect()
 
     temp_file.close()
@@ -298,7 +319,8 @@ def process_midas_supplement(update=False, verbose=False):
     return supplement_data
 
 
-def dump_midas_supplement_to_mssql(table_name='MIDAS_RADTOB_suppl', if_exists='replace', update=False, verbose=False):
+def dump_midas_supplement_to_mssql(table_name='MIDAS_RADTOB_suppl', if_exists='replace',
+                                   update=False, verbose=False):
     """
 
     :param table_name:
@@ -318,14 +340,16 @@ def dump_midas_supplement_to_mssql(table_name='MIDAS_RADTOB_suppl', if_exists='r
 
     if supplement_data is not None and not supplement_data.empty:
         midas_radtob_engine = create_mssql_connectable_engine(database_name='Weather')
-        supplement_data.to_sql(table_name, midas_radtob_engine, schema='dbo', if_exists=if_exists, index=False,
-                               dtype={'OB_END_DATE': sqlalchemy.types.DATE})
+        supplement_data.to_sql(table_name, midas_radtob_engine, schema='dbo', if_exists=if_exists,
+                               index=False, dtype={'OB_END_DATE': sqlalchemy.types.DATE})
 
 
-def query_midas_radtob_by_grid_datetime(met_stn_id, period, route_name, use_suppl_dat=False, update=False, dat_dir=None,
-                                        pickle_it=False, verbose=False):
+def query_midas_radtob_by_grid_datetime(met_stn_id, period, route_name, use_suppl_dat=False,
+                                        update=False, dat_dir=None, pickle_it=False,
+                                        verbose=False):
     """
-    Get MIDAS RADTOB (Radiation data) by met station ID (Query from the database) for the given ``period``.
+    Get MIDAS RADTOB (Radiation data) by met station ID (Query from the database)
+    for the given ``period``.
 
     :param met_stn_id: met station ID
     :type met_stn_id: list
@@ -335,13 +359,15 @@ def query_midas_radtob_by_grid_datetime(met_stn_id, period, route_name, use_supp
     :type route_name: str
     :param use_suppl_dat: defaults to ``False``
     :type use_suppl_dat: bool
-    :param update: whether to check on update and proceed to update the package data, defaults to ``False``
+    :param update: whether to check on update and proceed to update the package data,
+        defaults to ``False``
     :type update: bool
     :param dat_dir: directory where the queried data is saved, defaults to ``None``
     :type dat_dir: str, None
     :param pickle_it: whether to save the queried data as a pickle file, defaults to ``True``
     :type pickle_it: bool
-    :param verbose: whether to print relevant information in console as the function runs, defaults to ``False``
+    :param verbose: whether to print relevant information in console as the function runs,
+        defaults to ``False``
     :type verbose: bool, int
     :return: UKCP09 data by ``grids`` and ``period``
     :rtype: pandas.DataFrame
@@ -364,7 +390,8 @@ def query_midas_radtob_by_grid_datetime(met_stn_id, period, route_name, use_supp
         met_stn_id = incidents.Met_SRC_ID.iloc[3]
         period = incidents.Critical_Period.iloc[3]
         route_name = incidents.Route.iloc[3]
-        midas_radtob = query_midas_radtob_by_grid_datetime(met_stn_id, period, pickle_it=pickle_it, verbose=verbose)
+        midas_radtob = query_midas_radtob_by_grid_datetime(met_stn_id, period, pickle_it=pickle_it,
+                                                           verbose=verbose)
     """
 
     p_start, p_end = period.left.min().strftime('%Y%m%d%H'), period.right.max().strftime('%Y%m%d%H')
@@ -373,7 +400,8 @@ def query_midas_radtob_by_grid_datetime(met_stn_id, period, route_name, use_supp
     pickle_filename = "{}-{}.pickle".format(str(met_stn_id[0]), "-".join([p_start, p_end]))
 
     # Specify a directory/path to store the pickle file (if appropriate)
-    dat_dir = dat_dir if isinstance(dat_dir, str) and os.path.isabs(dat_dir) else cdd_weather("midas", "dat")
+    dat_dir = dat_dir if isinstance(dat_dir, str) and os.path.isabs(dat_dir) \
+        else cdd_weather("midas", "dat")
     path_to_pickle = cdd_weather(dat_dir, pickle_filename)
 
     if os.path.isfile(path_to_pickle) and not update:
@@ -384,10 +412,12 @@ def query_midas_radtob_by_grid_datetime(met_stn_id, period, route_name, use_supp
         conn_metex = create_mssql_connectable_engine(database_name='Weather')
         # Specify database sql query
         ms_id = met_stn_id[0]
-        dates = tuple(x.strftime('%Y-%m-%d %H:%M:%S') for x in [period.left.min(), period.right.max()])
+        dates = tuple(x.strftime('%Y-%m-%d %H:%M:%S')
+                      for x in [period.left.min(), period.right.max()])
         sql_query = "SELECT * FROM dbo.[MIDAS_RADTOB] " \
                     "WHERE [SRC_ID] = {} " \
-                    "AND [OB_END_DATE_TIME] BETWEEN '{}' AND '{}';".format(ms_id, dates[0], dates[1])
+                    "AND [OB_END_DATE_TIME] BETWEEN '{}' AND '{}';".format(ms_id,
+                                                                           dates[0], dates[1])
         # Query the weather data
         midas_radtob = pd.read_sql(sql_query, conn_metex)
 
@@ -395,7 +425,8 @@ def query_midas_radtob_by_grid_datetime(met_stn_id, period, route_name, use_supp
             dates = tuple(x.strftime('%Y-%m-%d') for x in [period.left.min(), period.right.max()])
             sql_query = "SELECT * FROM dbo.[MIDAS_RADTOB_suppl] " \
                         "WHERE [Route] = '{}' " \
-                        "AND [OB_END_DATE] BETWEEN '{}' AND '{}';".format(route_name, dates[0], dates[1])
+                        "AND [OB_END_DATE] BETWEEN '{}' AND '{}';".format(route_name,
+                                                                          dates[0], dates[1])
             midas_radtob = pd.read_sql(sql_query, conn_metex)
 
         if pickle_it:
