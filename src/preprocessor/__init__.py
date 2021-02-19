a50bb8135ed5f0dadd724d9bf9f9bb4475327547
mikeqfu <qian.fu@outlook.com>
2021-02-19 21:14:51 +0000

    Create a sub-package preprocessor

---
+++ src/preprocessor/__init__.py
@@ -0,0 +1,12 @@
+"""
+Preprocessor
+"""
+
+from .reports import Schedule8IncidentsSpreadsheet
+from .vegetation import Vegetation
+from .weather import METEX, WeatherThresholds, MIDAS, UKCP09
+
+
+__all__ = ['reports', 'Schedule8IncidentsSpreadsheet',
+           'vegetation', 'Vegetation',
+           'weather', 'METEX', 'WeatherThresholds', 'MIDAS', 'UKCP09']
