168b7cf9d2f78957b753267e4ec0365cc2907e39
mikeqfu <qian.fu@outlook.com>
2021-03-04 08:33:45 +0000

    Update preprocessor\__init__.py

--- src/preprocessor/__init__.py
+++ src/preprocessor/__init__.py
@@ -2,7 +2,7 @@
 Preprocessor
 """
 
-from .metex import DelayAttributionGlossary, METExLite, WeatherThresholds, Schedule8IncidentReports
+from .metex import DelayAttributionGlossary, METExLite, Schedule8IncidentReports, WeatherThresholds
 from .network import Anglia
 from .vegetation import Vegetation
 from .weather import MIDAS, UKCP09
@@ -11,5 +11,5 @@ __all__ = [
     'network', 'Anglia',
     'metex', 'METExLite', 'WeatherThresholds', 'Schedule8IncidentReports', 'DelayAttributionGlossary',
     'vegetation', 'Vegetation',
-    'weather', 'MIDAS', 'UKCP09'
+    'weather', 'MIDAS', 'UKCP09',
 ]
