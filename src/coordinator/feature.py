9a48187e298739702cb1f5092f05fd3d9338dcf7
mikeqfu <qian.fu@outlook.com>
2021-03-10 22:43:33 +0000

    Modify coordinator\feature.py

--- src/coordinator/feature.py
+++ src/coordinator/feature.py
@@ -7,25 +7,39 @@ import pandas as pd
 from pyhelpers.text import find_similar_str
 
 
-def get_data_by_season(incident_data, season='summer'):
+def get_data_by_meteorological_seasons(incident_records, in_seasons, datetime_col):
     """
-    Get data for specified season(s).
+    Get data for a meteorological season or seasons.
 
-    :param incident_data:
-    :type incident_data: pandas.DataFrame
-    :param season: 'spring', 'summer', 'autumn', 'winter'; if ``None``, returns data of all seasons
-    :type season: str, None
+    The meteorological start of a season is based on the annual temperature cycle and
+    the 12-month calendar. According to this definition, each season begins on the first of a
+    particular month and lasts for three months:
+
+        - Spring begins: 1 March
+        - Summer begins: 1 June
+        - Autumn begins: 1 September
+        - Winter begins: 1 December
+
+    (Source: https://www.almanac.com/content/first-day-seasons)
+
+    :param incident_records:
+    :type incident_records: pandas.DataFrame
+    :param in_seasons: 'spring', 'summer', 'autumn', 'winter'; if ``None``, returns data of all seasons
+    :type in_seasons: str or list or None
+    :param datetime_col:
     :return:
     :rtype: pandas.DataFrame
     """
 
-    assert season is None or season in ('spring', 'summer', 'autumn', 'winter')
+    if in_seasons:
+        default_season_names = ['spring', 'summer', 'autumn', 'winter']
+        input_season_names = [in_seasons] if isinstance(in_seasons, str) else in_seasons
+        valid_names = [find_similar_str(s, default_season_names) for s in input_season_names]
 
-    if season:
         spring_data, summer_data, autumn_data, winter_data = [pd.DataFrame()] * 4
 
-        for y in pd.unique(incident_data.FinancialYear):
-            y_dat = incident_data[incident_data.FinancialYear == y]
+        for y in pd.unique(incident_records.FinancialYear):
+            y_dat = incident_records[incident_records.FinancialYear == y]
 
             # Get data for spring -----------------------------------
             spring_start1 = datetime.datetime(year=y, month=4, day=1)
@@ -35,87 +49,106 @@ def get_data_by_season(incident_data, season='summer'):
             spring_dates1 = pd.date_range(spring_start1, spring_start1 + pd.DateOffset(months=2))
             spring_dates2 = pd.date_range(spring_start2, spring_start2 + pd.DateOffset(months=2))
             spring_data = pd.concat([spring_data, y_dat[
-                y_dat.StartDateTime.isin(spring_dates1) | y_dat.StartDateTime.isin(spring_dates2)]])
+                y_dat[datetime_col].isin(spring_dates1) | y_dat[datetime_col].isin(spring_dates2)]])
 
             spring_time = \
-                ((y_dat.StartDateTime >= spring_start1) & (y_dat.StartDateTime < spring_end1)) | \
-                ((y_dat.StartDateTime >= spring_start2) & (y_dat.StartDateTime < spring_end2))
+                ((y_dat[datetime_col] >= spring_start1) & (y_dat[datetime_col] < spring_end1)) | \
+                ((y_dat[datetime_col] >= spring_start2) & (y_dat[datetime_col] < spring_end2))
             spring_data = pd.concat([spring_data, y_dat.loc[spring_time]])
+
             # Get data for summer ----------------------------------
             summer_start = datetime.datetime(year=y, month=6, day=1)
             summer_end = summer_start + pd.DateOffset(months=3)
-            summer = (y_dat.StartDateTime >= summer_start) & (y_dat.StartDateTime < summer_end)
+            summer = (y_dat[datetime_col] >= summer_start) & (y_dat[datetime_col] < summer_end)
             summer_data = pd.concat([summer_data, y_dat.loc[summer]])
+
             # Get data for autumn ----------------------------------
             autumn_start = datetime.datetime(year=y, month=9, day=1)
             autumn_end = autumn_start + pd.DateOffset(months=3)
-            autumn = (y_dat.StartDateTime >= autumn_start) & (y_dat.StartDateTime < autumn_end)
+            autumn = (y_dat[datetime_col] >= autumn_start) & (y_dat[datetime_col] < autumn_end)
             autumn_data = pd.concat([autumn_data, y_dat.loc[autumn]])
+
             # Get data for winter -----------------------------------
             winter_start = datetime.datetime(year=y, month=12, day=1)
             winter_end = winter_start + pd.DateOffset(months=3)
-            winter = (y_dat.StartDateTime >= winter_start) & (y_dat.StartDateTime < winter_end)
+            winter = (y_dat[datetime_col] >= winter_start) & (y_dat[datetime_col] < winter_end)
             winter_data = pd.concat([winter_data, y_dat.loc[winter]])
 
-        seasons = ['spring', 'summer', 'autumn', 'winter']
-        season = [season] if isinstance(season, str) else season
-        season = [find_similar_str(s, seasons) for s in season]
-        seasonal_data = eval(
-            "pd.concat([%s], ignore_index=True)" % ', '.join(['{}_data'.format(s) for s in season]))
+        season_data = eval(
+            "pd.concat([%s], ignore_index=True)" % ', '.join(['{}_data'.format(s) for s in valid_names]))
 
-        return seasonal_data
+        return season_data
 
     else:
-        return incident_data
+        return incident_records
 
 
-def get_data_by_season_(mod_data, in_seasons, incident_datetime_col):
+def get_data_by_astronomical_seasons(mod_data, in_seasons, datetime_col):
     """
-    An alternative to ``get_data_by_season()``.
+    Get data for a astronomical season or seasons.
+
+    The astronomical start of a season is based on the position of the Earth in relation to the Sun.
+    More specifically, the start of each season is marked by either a solstice (for winter and summer)
+    or an equinox (for spring and autumn).
+    A solstice is when the Sun reaches the most southerly or northerly point in the sky,
+    while an equinox is when the Sun passes over Earth’s equator.
+    Because of leap years, the dates of the equinoxes and solstices can shift by a day or two over time,
+    causing the start dates of the seasons to shift, too.
+
+        - Spring begins: 19 or 20 March
+        - Summer begins: 20 or 21 June
+        - Autumn begins: 22 or 23 September
+        - Winter begins: 21 or 22 December
+
+    (Source: https://www.almanac.com/content/first-day-seasons)
 
     :param mod_data:
     :type mod_data: pandas.DataFrame
-    :param in_seasons: 'spring', 'summer', 'autumn', 'winter';
-        if ``None``, returns data of all seasons
-    :type in_seasons: str, None
-    :param incident_datetime_col:
-    :type incident_datetime_col: str
+    :param in_seasons: 'spring', 'summer', 'autumn', 'winter'; if ``None``, all available data
+    :type in_seasons: str or list or None
+    :param datetime_col:
+    :type datetime_col: str
     :return:
     :rtype: pandas.DataFrame
     """
 
     if in_seasons is None:
-        return mod_data
+        season_data = mod_data.copy()
+
     else:
-        seasons = [in_seasons] if isinstance(in_seasons, str) else in_seasons
-        selected_seasons = [find_similar_str(s, ('Spring', 'Summer', 'Autumn', 'Winter'))
-                            for s in seasons]
+        default_season_names = ['spring', 'summer', 'autumn', 'winter']
+
+        input_season_names = [in_seasons] if isinstance(in_seasons, str) else in_seasons
+        selected_seasons = [find_similar_str(s, default_season_names) for s in input_season_names]
 
         def identify_season(incident_dt):
             """
             # (incident_datetime.dt.month % 12 + 3) // 3
             """
+
             y = incident_dt.year
             seasons_dt = [
-                ('Winter', (datetime.datetime(y, 1, 1),
+                ('winter', (datetime.datetime(y, 1, 1),
                             datetime.datetime(y, 3, 20) + pd.Timedelta(1, unit='day'))),
-                ('Spring', (datetime.datetime(y, 3, 21),
+                ('spring', (datetime.datetime(y, 3, 21),
                             datetime.datetime(y, 6, 20) + pd.Timedelta(1, unit='day'))),
-                ('Summer', (datetime.datetime(y, 6, 21),
+                ('summer', (datetime.datetime(y, 6, 21),
                             datetime.datetime(y, 9, 22) + pd.Timedelta(1, unit='day'))),
-                ('Autumn', (datetime.datetime(y, 9, 23),
+                ('autumn', (datetime.datetime(y, 9, 23),
                             datetime.datetime(y, 12, 20) + pd.Timedelta(1, unit='day'))),
-                ('Winter', (datetime.datetime(y, 12, 21),
+                ('winter', (datetime.datetime(y, 12, 21),
                             datetime.datetime(y, 12, 31) + pd.Timedelta(1, unit='day')))]
+
             return next(season for season, (start, end) in seasons_dt if start <= incident_dt < end)
 
-        mod_data_seasons = mod_data[incident_datetime_col].map(identify_season)
+        mod_data_seasons = mod_data[datetime_col].map(identify_season)
+
         season_data = mod_data[mod_data_seasons.isin(selected_seasons)]
 
-        return season_data
+    return season_data
 
 
-def categorise_wind_directions(degree):
+def define_wind_direction(degree):
     """
     Categorise wind directions into four quadrants.
 
@@ -135,95 +168,9 @@ def categorise_wind_directions(degree):
         return 4
 
 
-def categorise_track_orientations(data, start_lon_colname='StartLongitude',
-                                  start_lat_colname='StartLatitude',
-                                  end_lon_colname='EndLongitude',
-                                  end_lat_colname='EndLatitude'):
+def define_track_orientation(lon1, lat1, lon2, lat2):
     """
-    Categorise track orientations.
-
-    :param data:
-    :type data:
-    :param start_lon_colname: column name of start longitude
-    :type start_lon_colname: str
-    :param start_lat_colname: column name of start latitude
-    :type start_lat_colname: str
-    :param end_lon_colname: column name of end longitude
-    :type end_lon_colname: str
-    :param end_lat_colname: column name of end latitude
-    :type end_lat_colname: str
-    :return:
-    :rtype:
-
-    **Test**::
-
-        start_lon_colname = 'StartLongitude'
-        start_lat_colname = 'StartLatitude'
-        end_lon_colname = 'EndLongitude'
-        end_lat_colname = 'EndLatitude'
-
-        data = incident_location_weather.copy()
-
-    """
-
-    track_orientations = pd.DataFrame(None, index=range(len(data)), columns=['Track_Orientation'])
-    categories = ['N_S', 'NE_SW', 'NW_SE', 'E_W']
-    # origin = (-0.565409, 51.23622)
-    start_lon, start_lat = data[start_lon_colname], data[start_lat_colname]
-    end_lon, end_lat = data[end_lon_colname], data[end_lat_colname]
-    track_orientations['Track_Orientation_radians'] = np.arctan2(
-        end_lat - start_lat, end_lon - start_lon)  # [-pi, pi]
-
-    # N-S / S-N: [-np.pi*2/3, -np.pi/3] & [np.pi/3, np.pi*2/3]
-    n_s = np.logical_or(
-        np.logical_and(track_orientations.Track_Orientation_radians >= -np.pi * 2 / 3,
-                       track_orientations.Track_Orientation_radians < -np.pi / 3),
-        np.logical_and(track_orientations.Track_Orientation_radians >= np.pi / 3,
-                       track_orientations.Track_Orientation_radians < np.pi * 2 / 3))
-    track_orientations.loc[n_s, 'Track_Orientation'] = categories[0]
-
-    # NE-SW / SW-NE: [np.pi/6, np.pi/3] & [-np.pi*5/6, -np.pi*2/3]
-    ne_sw = np.logical_or(
-        np.logical_and(track_orientations.Track_Orientation_radians >= np.pi / 6,
-                       track_orientations.Track_Orientation_radians < np.pi / 3),
-        np.logical_and(track_orientations.Track_Orientation_radians >= -np.pi * 5 / 6,
-                       track_orientations.Track_Orientation_radians < -np.pi * 2 / 3))
-    track_orientations.loc[ne_sw, 'Track_Orientation'] = categories[1]
-
-    # NW-SE / SE-NW: [np.pi*2/3, np.pi*5/6], [-np.pi/3, -np.pi/6]
-    nw_se = np.logical_or(
-        np.logical_and(track_orientations.Track_Orientation_radians >= np.pi * 2 / 3,
-                       track_orientations.Track_Orientation_radians < np.pi * 5 / 6),
-        np.logical_and(track_orientations.Track_Orientation_radians >= -np.pi / 3,
-                       track_orientations.Track_Orientation_radians < -np.pi / 6))
-    track_orientations.loc[nw_se, 'Track_Orientation'] = categories[2]
-
-    # E-W / W-E: [-np.pi, -np.pi*5/6], [-np.pi/6, np.pi/6], [np.pi*5/6, np.pi]
-    track_orientations.Track_Orientation.fillna(categories[3], inplace=True)
-    # e_w = np.logical_or(np.logical_or(
-    #     np.logical_and(df.Track_Orientation_radians >= -np.pi,
-    #                    df.Track_Orientation_radians < -np.pi * 5 / 6),
-    #     np.logical_and(df.Track_Orientation_radians >= -np.pi/6,
-    #                    df.Track_Orientation_radians < np.pi/6)),
-    #     np.logical_and(df.Track_Orientation_radians >= np.pi*5/6,
-    #                    df.Track_Orientation_radians < np.pi))
-    # data.Track_Orientation[e_w] = 'E_W'
-
-    prefix, sep = 'Track_Orientation', '_'
-    categorical_var = pd.get_dummies(
-        track_orientations.Track_Orientation, prefix=prefix, prefix_sep=sep)
-    categorical_var = categorical_var.T.reindex(
-        [prefix + sep + x for x in categories]).T.fillna(0).astype(np.int64)
-
-    track_orientations = pd.concat(
-        [track_orientations[['Track_Orientation']], categorical_var], axis=1)
-
-    return track_orientations
-
-
-def categorise_track_orientations_(lon1, lat1, lon2, lat2):
-    """
-    An alternative to ``categorise_track_orientations()``.
+    Categorise the orientation of a track.
 
     :param lon1: longitude (of start location)
     :type lon1:
@@ -238,6 +185,7 @@ def categorise_track_orientations_(lon1, lat1, lon2, lat2):
     """
 
     track_orientation_radians = np.arctan2(lat2 - lat1, lon2 - lon1)  # Angles in radians, [-pi, pi]
+
     if np.logical_or(
             np.logical_and(track_orientation_radians >= -np.pi * 2 / 3,
                            track_orientation_radians < -np.pi / 3),
@@ -245,6 +193,7 @@ def categorise_track_orientations_(lon1, lat1, lon2, lat2):
                            track_orientation_radians < np.pi * 2 / 3)):
         # N-S / S-N: [-np.pi*2/3, -np.pi/3] & [np.pi/3, np.pi*2/3]
         track_orientation = 'N_S'
+
     elif np.logical_or(
             np.logical_and(track_orientation_radians >= np.pi / 6,
                            track_orientation_radians < np.pi / 3),
@@ -252,12 +201,14 @@ def categorise_track_orientations_(lon1, lat1, lon2, lat2):
                            track_orientation_radians < -np.pi * 2 / 3)):
         # NE-SW / SW-NE: [np.pi/6, np.pi/3] & [-np.pi*5/6, -np.pi*2/3]
         track_orientation = 'NE_SW'
+
     elif np.logical_or(
             np.logical_and(track_orientation_radians >= np.pi * 2 / 3,
                            track_orientation_radians < np.pi * 5 / 6),
             np.logical_and(track_orientation_radians >= -np.pi / 3,
                            track_orientation_radians < -np.pi / 6)):
         track_orientation = 'NW_SE'
+
     else:
         # np.logical_or(np.logical_or(
         #     np.logical_and(track_orientation_radians >= -np.pi,
@@ -267,7 +218,97 @@ def categorise_track_orientations_(lon1, lat1, lon2, lat2):
         #     np.logical_and(track_orientation_radians >= np.pi*5/6,
         #                    track_orientation_radians < np.pi))
         track_orientation = 'E_W'
+
     return track_orientation
+
+
+def categorise_track_orientations(data, start_lon_colname='StartLongitude',
+                                  start_lat_colname='StartLatitude',
+                                  end_lon_colname='EndLongitude',
+                                  end_lat_colname='EndLatitude', col_name='Track_Orientation'):
+    """
+    Categorise track orientations.
+
+    :param data:
+    :type data:
+    :param start_lon_colname: column name of start longitude
+    :type start_lon_colname: str
+    :param start_lat_colname: column name of start latitude
+    :type start_lat_colname: str
+    :param end_lon_colname: column name of end longitude
+    :type end_lon_colname: str
+    :param end_lat_colname: column name of end latitude
+    :type end_lat_colname: str
+    :param col_name:
+    :return:
+    :rtype:
+
+    **Test**::
+
+        start_lon_colname = 'StartLongitude'
+        start_lat_colname = 'StartLatitude'
+        end_lon_colname = 'EndLongitude'
+        end_lat_colname = 'EndLatitude'
+
+        data = incident_location_weather.copy()
+
+    """
+
+    track_orientations = pd.DataFrame(None, index=range(len(data)), columns=[col_name])
+
+    categories = ['N_S', 'NE_SW', 'NW_SE', 'E_W']
+
+    # origin = (-0.565409, 51.23622)
+    start_lon, start_lat = data[start_lon_colname], data[start_lat_colname]
+    end_lon, end_lat = data[end_lon_colname], data[end_lat_colname]
+    # [-pi, pi]
+    track_orientations[col_name + '_radians'] = np.arctan2(end_lat - start_lat, end_lon - start_lon)
+
+    # N-S / S-N: [-np.pi*2/3, -np.pi/3] & [np.pi/3, np.pi*2/3]
+    n_s = np.logical_or(
+        np.logical_and(track_orientations.Track_Orientation_radians >= -np.pi * 2 / 3,
+                       track_orientations.Track_Orientation_radians < -np.pi / 3),
+        np.logical_and(track_orientations.Track_Orientation_radians >= np.pi / 3,
+                       track_orientations.Track_Orientation_radians < np.pi * 2 / 3))
+    track_orientations.loc[n_s, col_name] = categories[0]
+
+    # NE-SW / SW-NE: [np.pi/6, np.pi/3] & [-np.pi*5/6, -np.pi*2/3]
+    ne_sw = np.logical_or(
+        np.logical_and(track_orientations.Track_Orientation_radians >= np.pi / 6,
+                       track_orientations.Track_Orientation_radians < np.pi / 3),
+        np.logical_and(track_orientations.Track_Orientation_radians >= -np.pi * 5 / 6,
+                       track_orientations.Track_Orientation_radians < -np.pi * 2 / 3))
+    track_orientations.loc[ne_sw, col_name] = categories[1]
+
+    # NW-SE / SE-NW: [np.pi*2/3, np.pi*5/6], [-np.pi/3, -np.pi/6]
+    nw_se = np.logical_or(
+        np.logical_and(track_orientations.Track_Orientation_radians >= np.pi * 2 / 3,
+                       track_orientations.Track_Orientation_radians < np.pi * 5 / 6),
+        np.logical_and(track_orientations.Track_Orientation_radians >= -np.pi / 3,
+                       track_orientations.Track_Orientation_radians < -np.pi / 6))
+    track_orientations.loc[nw_se, col_name] = categories[2]
+
+    # E-W / W-E: [-np.pi, -np.pi*5/6], [-np.pi/6, np.pi/6], [np.pi*5/6, np.pi]
+    track_orientations[col_name].fillna(categories[3], inplace=True)
+    # e_w = np.logical_or(np.logical_or(
+    #     np.logical_and(df.Track_Orientation_radians >= -np.pi,
+    #                    df.Track_Orientation_radians < -np.pi * 5 / 6),
+    #     np.logical_and(df.Track_Orientation_radians >= -np.pi/6,
+    #                    df.Track_Orientation_radians < np.pi/6)),
+    #     np.logical_and(df.Track_Orientation_radians >= np.pi*5/6,
+    #                    df.Track_Orientation_radians < np.pi))
+    # data[col_name][e_w] = 'E_W'
+
+    prefix, sep = col_name, '_'
+    categorical_var = pd.get_dummies(
+        track_orientations[col_name], prefix=prefix, prefix_sep=sep)
+    categorical_var = categorical_var.T.reindex(
+        [prefix + sep + x for x in categories]).T.fillna(0).astype(np.int64)
+
+    track_orientations = pd.concat(
+        [track_orientations[[col_name]], categorical_var], axis=1)
+
+    return track_orientations
 
 
 def categorise_temperatures(attr_dat, column_name='Temperature_max'):
