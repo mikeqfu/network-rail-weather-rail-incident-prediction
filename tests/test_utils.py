44cd9c702e66a9b893d3ce4db1b7cfbf7aca2ed7
Qian Fu <q.fu@bham.ac.uk>
2025-04-25 22:34:04 +0100

    Modify test submodule: `test_utils`

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,10 +4,10 @@ Test the subpackage ``src.utils``.
 
 import pytest
 
-from src.utils import *
-
 
 def test_make_filename():
+    from src.utils import make_filename
+
     name = "filename"  # None
 
     route_name, weather_category = None, None
@@ -16,19 +16,21 @@ def test_make_filename():
 
     route_name, weather_category = None, 'Heat'
     filename = make_filename(None, route_name, weather_category, "test1")
-    assert filename == 'Heat_test1.pkl'
+    assert filename == 'Heat-test1.pkl'
 
     filename = make_filename(name, route_name, weather_category, "test1", "test2")
-    assert filename == 'filename_Heat_test1_test2.pkl'
+    assert filename == 'filename-Heat-test1-test2.pkl'
 
     filename = make_filename(name, 'Anglia', weather_category, "test2")
-    assert filename == 'filename_Anglia_Heat_test2.pkl'
+    assert filename == 'filename-Anglia-Heat-test2.pkl'
 
     filename = make_filename(name, 'North and East', 'Heat', "test1", "test2")
-    assert filename == 'filename_N&E_Heat_test1_test2.pkl'
+    assert filename == 'filename-N&E-Heat-test1-test2.pkl'
 
 
 def test_get_index_of_dict_in_list():
+    from src.utils import get_index_of_dict_in_list
+
     lst_of_dict = [{'a': 1}, {'b': 2}, {'c': 3}]
     key = 'b'
     val = 2
